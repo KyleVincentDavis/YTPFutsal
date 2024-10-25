aebde1e8292cf9db25974bedb6bfa3b7bbe63247
Kyle <kylevincent.davis24@gmail.com>
2024-10-25 13:04:09 +0800

    Your commit message here

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add 'scroll' class to body on scroll
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-      document.body.classList.add('scroll');
-    } else {
-      document.body.classList.remove('scroll');
-    }
-  });
-  
